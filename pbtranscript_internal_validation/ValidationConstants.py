dfc829c812a2dd5eb47a795428558705f59b88ba
Yuan Li <yli@pacificbiosciences.com>
2017-05-19 12:10:40 -0700

    add fasta files of SIRV genome and isoform

--- pbtranscript_internal_validation/ValidationConstants.py
+++ pbtranscript_internal_validation/ValidationConstants.py
@@ -16,4 +16,7 @@ class ValidationConstants(object):
     SIRV_GMAP_NAME = "SIRV"
     GMAP_NPROC = 12
     SIRV_NAME = "SIRV"
-    SIRV_TRUTH_DIR = "/pbi/dept/secondary/siv/testdata/isoseq/lexigoen-ground-truth/validation"
+    SIRV_DATA_ROOT_DIR = "/pbi/dept/secondary/siv/testdata/isoseq/lexigoen-ground-truth"
+    SIRV_TRUTH_DIR = op.join(SIRV_DATA_ROOT_DIR, 'validation')
+    SIRV_ISOFORMS_FA = op.join(SIRV_DATA_ROOT_DIR, 'isoforms', 'SIRV_C_150601a.truth.fasta')
+    SIRV_GENOME_FA = op.join(SIRV_DATA_ROOT_DIR, 'reference', 'SIRV_150601a.fasta')
