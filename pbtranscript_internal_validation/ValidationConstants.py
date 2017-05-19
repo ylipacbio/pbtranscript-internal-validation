4bf290c5f07da4f43af12a69c001795847d7ed6e
Yuan Li <yli@pacificbiosciences.com>
2017-05-19 13:36:43 -0700

    Add fasta of human transcripts

--- pbtranscript_internal_validation/ValidationConstants.py
+++ pbtranscript_internal_validation/ValidationConstants.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 """Define constants of data resources to use for validation."""
+import os.path as op
 
 
 __all__ = [ "ValidationConstants" ]
@@ -9,8 +10,10 @@ __all__ = [ "ValidationConstants" ]
 class ValidationConstants(object):
 
     """ Constants of validation resources and parameter constants"""
+    GENCODE_ROOT_DIR = "/pbi/dept/secondary/siv/testdata/isoseq/gencode"
+    GENCODE_GTF = op.join(GENCODE_ROOT_DIR, "gencode.v25.annotation.gtf")
+    HUMAN_TRANSCRIPTS_FA = op.join(GENCODE_ROOT_DIR, "gencode.v25.transcripts.fa")
 
-    GENCODE_GTF = "/pbi/dept/secondary/siv/testdata/isoseq/gencode/gencode.v25.annotation.gtf"
     GMAP_DB = "/pbi/dept/secondary/siv/testdata/isoseq/gmap_db/"
     HUMAN_GMAP_NAME = "hg38"
     SIRV_GMAP_NAME = "SIRV"
@@ -18,5 +21,5 @@ class ValidationConstants(object):
     SIRV_NAME = "SIRV"
     SIRV_DATA_ROOT_DIR = "/pbi/dept/secondary/siv/testdata/isoseq/lexigoen-ground-truth"
     SIRV_TRUTH_DIR = op.join(SIRV_DATA_ROOT_DIR, 'validation')
-    SIRV_ISOFORMS_FA = op.join(SIRV_DATA_ROOT_DIR, 'isoforms', 'SIRV_C_150601a.truth.fasta')
+    SIRV_TRANSCRIPTS_FA = op.join(SIRV_DATA_ROOT_DIR, 'isoforms', 'SIRV_C_150601a.truth.fasta')
     SIRV_GENOME_FA = op.join(SIRV_DATA_ROOT_DIR, 'reference', 'SIRV_150601a.fasta')
